e141a35920f5a514014c3545e4b6479ddeca3e34
mqprichard <mqprichard@gmail.com>
2012-09-12 08:28:42 -0700

    Added scripts to play complete Spassky-Fischer Reykjavik 13 against a local CB Tomcat server

---
+++ scripts/playSpasskyFischer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+jsonElement(){
+        #echo $1
+        out=$(echo $2 | awk -F"[,:]" '{for(i=1;i<=NF;i++){if($i~/'$1'\042/){print $(i+1)} } }')
+        if [[ $out == \"*\" ]]; #test if the string starts and ends in a quote
+        then
+            echo "${out:1:${#out}-2}" #removes first and last quotes
+        elif [[  $out == \"*\"} ]];
+        then
+            echo "${out:1:${#out}-3}" #removes first and last quotes plus the curly brace
+        else
+            echo $out
+        fi
+}
+
+newgame=`curl -s -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d '{"white":"Spassky","black":"Fischer","description":"Reykjavik Game 13"}' http://localhost:8080/chess/game/new`
+gameid=$(jsonElement "id" $newgame)
+
+sed s/GAME/$gameid/g templateSpasskyFischer.sh > tmpSpasskyFischer.sh
+sed s/GAME/$gameid/g templateSpasskyFischer.out > tmpSpasskyFischer.out
+
+chmod u+x tmpSpasskyFischer.sh
+
+diff <(./tmpSpasskyFischer.sh) tmpSpasskyFischer.out
+rm -f tmpSpasskyFischer.sh tmpSpasskyFischer.out
+
+curl -H "Accept: application/json" -X GET http://localhost:8080/chess/game/$gameid; echo
+curl -H "Accept: application/json" -X GET http://localhost:8080/chess/moves/$gameid; echo
+ 
